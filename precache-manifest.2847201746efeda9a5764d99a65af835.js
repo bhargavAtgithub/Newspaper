5d50cd8897b0916158a08641251f7efaed730074
Bhargav <bhargav999reddy@gmail.com>
2020-05-22 15:47:30 +0530

    Updates

---
+++ precache-manifest.2847201746efeda9a5764d99a65af835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87379ae3d4eb7e2bca696ebe2f9d3866",
+    "url": "/Newspaper/index.html"
+  },
+  {
+    "revision": "7c138161c394ae7a8247",
+    "url": "/Newspaper/static/css/main.206b8b7e.chunk.css"
+  },
+  {
+    "revision": "bb1155c962fcc1a9732c",
+    "url": "/Newspaper/static/js/2.9af2066c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Newspaper/static/js/2.9af2066c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c138161c394ae7a8247",
+    "url": "/Newspaper/static/js/main.2363e27f.chunk.js"
+  },
+  {
+    "revision": "4dd2362cc5d47924fff7",
+    "url": "/Newspaper/static/js/runtime-main.864513cd.js"
+  }
+]);
